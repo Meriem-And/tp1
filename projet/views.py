b607578a393ad2499130dddb853f6b6bdbd7d1d1
Meriem-And <meryem.androussi@gmail.com>
2021-08-16 18:11:31 -0400

    Updating page html

--- projet/views.py
+++ projet/views.py
@@ -1,6 +1,6 @@
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello Kevin , this is my first page on Django ")
+    return render(request,"index.html")
