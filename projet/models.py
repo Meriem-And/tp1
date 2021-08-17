4a6d936b75eb4d82752af9c9bfb1b5f9c79aee14
Meriem-And <meryem.androussi@gmail.com>
2021-08-17 14:29:11 -0400

    creation base de donné

--- projet/models.py
+++ projet/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Projet(models.Model):
+    titre = models.CharField(max_length=50)
+    descriotion = models.TextField(max_length=1000)
+    image_url = models.CharField(max_length=2000)
