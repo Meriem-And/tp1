2689be8d4e1b0408e1e9275354bf9564bf43edea
Meriem-And <meryem.androussi@gmail.com>
2021-08-17 16:01:18 -0400

    construction des elements du site

--- projet/views.py
+++ projet/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
-
+from .models import Projet
 
 def index(request):
-    return render(request,"index.html")
+    projet= Projet.objects.all()
+    return render(request,"index.html",{"projet":projet})
